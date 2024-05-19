6f4da17f6f0ed50daecd3eb74b84a3dd5bdeac43
preetham.ntp <preetham.ntp@nexergroup.com>
2024-05-19 13:49:51 +0530

    todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import "./App.css"
+
+// type Props = {
+//   "id": number,
+//   "todo": string,
+//   "completed": boolean,
+//   "userId": number
+// }
 
 function App() {
+
+
+  const [todosData, setTodosData] = useState([])
+  const [insertTodo, setInsertTodo] = useState('')
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/todos").then((res) => {
+      return res.json()
+    }).then(data => {
+      setTodosData(data.todos)
+    }).catch(err => {
+      console.error(err);
+    })
+  }, [])
+
+
+  const handleCheck = (id) => {
+    setTodosData(todosData.map(item => item.id === id ? {
+      ...item,
+      completed: !item.completed
+    } : item
+    ))
+  }
+
+  const handleAddTodo = () => {
+
+    const newToDo = {
+      id: todosData.length+1,
+      todo: insertTodo,
+      completed: false,
+      userId: 27
+    }
+
+    setTodosData([newToDo,...todosData])
+  
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <input type='text' value={insertTodo} onChange={(e) => setInsertTodo(e.target.value)} />
+      <button onClick={handleAddTodo}> Add </button>
+      {todosData?.map(todo => <ul key={`${todo.userId}${todo.id}`}>
+        <li className={todo.completed ? "strike" : ""}>
+          <input type='checkbox' checked={todo.completed} onChange={() => handleCheck(todo.id)} />
+          {todo.todo}
+        </li>
+      </ul>)}
     </div>
   );
 }
